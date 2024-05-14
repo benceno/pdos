63c0dfb29b1758d5d94730da1f1c706ef7e240ad
JoaoVitorSD <joaovitorsantanadepollo@gmail.com>
2024-05-13 22:58:13 -0300

    feat: process type in sanity

--- sanity.c
+++ sanity.c
@@ -7,30 +7,62 @@
 #include "syscall.h"
 #include "traps.h"
 #include "memlayout.h"
-
+void empty_loop(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+    }
+}
+enum proctype
+{
+    CPU_BOUND,
+    S_BOUND,
+    IO_BOUND
+};
 int main(int argc, char *argv[])
 {
-    if(argc != 2){
-        printf(1, "Usage: sanity <number of processes>\n");
+    if (argc != 2)
+    {
+        printf(1, "Invalid number of arguments passed\n");
         exit();
     }
     int numberProcesses = atoi(argv[1]);
-    int amount = numberProcesses*3;
+    int amount = numberProcesses * 3;
 
-
-    for(int i = 0; i < amount; i++){
+    for (int i = 0; i < amount; i++)
+    {
         int pid = fork();
-        if(pid<0){
-            printf(1, "Fork failed\n");
-            exit();
+        if (pid == -1)
+        {
+            continue;
         }
-    }
-    int pid = 0;
-    while(pid < amount){
+        enum proctype type = i % 3;
+        switch (type)
+        {
+        case CPU_BOUND:
+            for (int j = 0; j < 100; j++)
+            {
+                empty_loop(10000);
+            }
+            break;
+        case S_BOUND:
+            for (int j = 0; j < 20; j++)
+            {
+                empty_loop(10000);
+            }
+            break;
+        case IO_BOUND:
+            for (int j = 0; j < 100; j++)
+            {
+                sleep(1);
+            }
+            break;
+        default:
+            break;
+        }
         int *retime = malloc(sizeof(int *)), *rutime = malloc(sizeof(int *)), *stime = malloc(sizeof(int *));
-        pid = wait2(retime, rutime, stime);
-        pid++;
-        // pid = wait2(&retime, &rutime, &stime);
+        wait2(retime, rutime, stime);
+        printf(1, "retime: %d, rutime: %d, stime: %d\n", *retime, *rutime, *stime);
     }
     return 0;
 }
