d596bb1953905c1c09e850341a02a4f53be1b1f2
JoaoVitorSD <joaovitorsantanadepollo@gmail.com>
2024-05-14 21:01:41 -0300

    fix: process forking awaiting

--- sanity.c
+++ sanity.c
@@ -65,7 +65,23 @@ void run_bound_loop(enum proctype type)
         break;
     }
 }
+typedef struct process_info
+{
+    int pid;
+    enum proctype type;
+} process_info;
 
+int get_process_index(int pid, process_info *process_pids, int numberProcesses)
+{
+    for (int i = 0; i < numberProcesses; i++)
+    {
+        if (process_pids[i].pid == pid)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -79,32 +95,47 @@ int main(int argc, char *argv[])
 
     int retime, rutime, stime;
     int total[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+
+    process_info *process_pids = malloc(numberProcesses * sizeof(process_info));
+    process_info process_info;
+    int processCreated = 0;
     for (int i = 0; i < numberProcesses; i++)
     {
         int pid = fork();
+        if (pid < 0)
+        {
+            printf(1, "fork failed\n");
+            break;
+        }
+        processCreated++;
         enum proctype type = i % 3;
         if (pid)
         {
-            if (wait2(&retime, &rutime, &stime) == pid)
-            {
-                printf(1, "pid: %d, type: %s, retime: %d, rutime: %d, stime: %d\n", i, proctype_string[type], retime, rutime, stime);
-                total[type][0] += retime;
-                total[type][1] += rutime;
-                total[type][2] += stime + rutime + retime;
-            }
-
+            process_info.pid = pid;
+            process_info.type = type;
+            process_pids[i] = process_info;
             continue;
         }
         run_bound_loop(type);
     }
-
+    for (int i = 0; i < processCreated; i++)
+    {
+        int wait_pid = wait2(&retime, &rutime, &stime);
+        int process_index = get_process_index(wait_pid, process_pids, numberProcesses);
+        process_info = process_pids[process_index];
+        enum proctype type = process_info.type;
+        printf(1, "pid: %d, type: %s, retime: %d, rutime: %d, stime: %d\n", process_info.pid, proctype_string[type], retime, rutime, stime);
+        total[type][0] += retime;
+        total[type][1] += rutime;
+        total[type][2] += stime + rutime + retime;
+    }
     printf(1, "Process averages\n");
     for (int i = 0; i < 3; i++)
     {
         printf(1, "type: %s, retime: %d, rutime: %d, turnaround: %d\n", proctype_string[i],
-               total[i][0] / numberProcesses,
-               total[i][1] / numberProcesses,
-               total[i][2] / numberProcesses);
+               total[i][0] / processCreated,
+               total[i][1] / processCreated,
+               total[i][2] / processCreated);
     }
     exit();
     return 0;
