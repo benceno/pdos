b392db20e89366ba5b800c697f300d38e837e0a9
Kim Jihwan <kjhwan0802@naver.com>
2024-05-24 17:11:44 +0900

    add test.c & mod Makefile

---
+++ test.c
@@ -0,0 +1,37 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "rand.c"
+
+int ticketget() {
+  //temp
+  return 1;
+}
+
+void exit_child(int pid) {
+  int p = wait();
+  printf(1, "process %d exited\n", p);
+}
+
+
+int main(int argc, char **argv)
+{
+  int p;
+
+  printf(1, "test start\n");
+  for (int i = 0; i < 20; i++) {
+    printf(1, "rand:%d\n", random());
+  }
+  p = fork();
+
+  if (p != 0) {
+    printf(1, "process %d my ticket %d\n", getpid(), ticketget());
+  }
+  else if (p == 0) {
+   // printf(1, "process %d my ticket %d\n", getpid(), ticketget());
+    exit();
+  }
+
+  exit_child(p);
+  exit();
+}
