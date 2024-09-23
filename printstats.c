11f722301f9bea172d02e858433cc0b1fba9af91
Pimyn Girgis <bemenboshra2001@gmail.com>
2024-09-23 20:07:42 +0200

    fix printstats

--- printstats.c
+++ printstats.c
@@ -1,14 +1,13 @@
-#include "types.h"
 #include "sort.h"
+#include "types.h"
 #include "user.h"
 
 void advance(char **str, const char c) {
-  while (**str && *((*str)++) != c){}
+  while (**str && *((*str)++) != c)
+    ;
 }
 
-int abs(int x) {
-  return x > 0? x : -x;
-}
+int abs(int x) { return x > 0 ? x : -x; }
 
 int sqrt_int(int target, int cur, int a, int b) {
   const int cur2 = cur * cur;
@@ -16,12 +15,11 @@ int sqrt_int(int target, int cur, int a, int b) {
   if (abs(diff) < 100 || a >= b) {
     return cur;
   }
-  return diff > 0? sqrt_int(target, (b + cur + 1)/2, cur + 1, b) : sqrt_int(target, (a + cur - 1)/2, a, cur - 1);
+  return diff > 0 ? sqrt_int(target, (b + cur + 1) / 2, cur + 1, b)
+                  : sqrt_int(target, (a + cur - 1) / 2, a, cur - 1);
 }
 
-int mysqrt(int val) {
-  return sqrt_int(val, val/2, 0, val);
-}
+int mysqrt(int val) { return sqrt_int(val, val / 2, 0, val); }
 
 int stdd(int list[], int n) {
   int sum = 0;
@@ -37,10 +35,10 @@ int stdd(int list[], int n) {
   return mysqrt(ret / n);
 }
 
-int printstats(int list[], int n) {
+void printstats(int list[], int n) {
   if (n == 0) {
     printf(2, "Empty List!\n");
-    return 1;
+    exit();
   }
   int sum = 0;
   for (int i = 0; i < n; ++i) {
@@ -52,7 +50,6 @@ int printstats(int list[], int n) {
   printf(1, "Median = %f\n", list[n / 2]);
   printf(1, "Min = %f\n", list[0]);
   printf(1, "Max = %f\n", list[n - 1]);
-  return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -62,19 +59,16 @@ int main(int argc, char *argv[]) {
     char *arg = argv[i + 1];
     list[i] = 100 * atoi(arg);
     advance(&arg, '.');
-    if (arg) {
-      int mul = 1;
+    int mul = 1;
+    if (*arg) {
       if (!arg[1]) {
         mul = 10;
       } else {
         arg[2] = 0;
       }
-      list[i] += mul * atoi(arg);
     }
-    printf(1, "%d\n", list[i]);
+    list[i] += mul * atoi(arg);
   }
-  if (printstats(list, argc)) {
-    return 1;
-  }
+  printstats(list, argc);
   exit();
 }
