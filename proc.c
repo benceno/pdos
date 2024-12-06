4baabe10c7f0c4801da82afbfcef4d860d9f275d
Tomer Rozen <tomerrzn@gmail.com>
2024-12-06 11:07:43 +0200

    add break for finished process

--- proc.c
+++ proc.c
@@ -344,6 +344,9 @@ scheduler(void)
         // Switch to chosen process.  It is the process's job
         // to release ptable.lock and then reacquire it
         // before jumping back to us.
+        if (p->state != RUNNABLE)
+            break
+
         c->proc = p;
         switchuvm(p);
         p->state = RUNNING;
